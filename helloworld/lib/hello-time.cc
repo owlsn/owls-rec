1454d29ba6a1474a741c344780f6aa5844861862
owlsn <helloworld.zouhao@gmail.com>
2019-12-01 21:51:01 +0800

    init

--- helloworld/lib/hello-time.cc
+++
@@ -1,6 +0,0 @@
-#include "lib/hello-time.h"
-
-void print_localtime(){
-  std::time_t result = std::time(nullptr);
-  std::cout << std::asctime(std::localtime(&result));
-}
--- helloworld/lib/hello-time.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIB_HELLO_TIME_H_
-#define LIB_HELLO_TIME_H_
-
-#include <ctime>
-#include <iostream>
-void print_localtime();
-
-#endif
